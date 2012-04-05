7ac1044a08e21bd22c3ffe7db1eebd0ad8204fcf
leocalzavara@gmail.com <leocalzavara@gmail.com@82dcad2b-1cc5-c1d1-2aa2-00431b11e489>
2012-04-05 23:06:25 +0000

    Base para o problema A do Qualification Round do Code Jam 2009.

---
+++ GoogleCodeJam/CodeJam2009.Qualification.A/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CodeJam2009.Qualification.A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = new StreamReader("SampleInput.txt");
+            var outputFile = new StreamWriter("Output.txt");
+
+            string[] line = inputFile.ReadLine().Split(' ');
+
+            int wordLength = Int32.Parse(line[0]);
+            int numberOfWords = Int32.Parse(line[1]);
+            int numberOfCases = Int32.Parse(line[2]);
+
+            for (int i = 0; i < numberOfCases; i++)
+            {
+                var caseMsg = String.Format("Case #{0}: ", i + 1);
+
+                Console.Write(caseMsg);
+                outputFile.Write(caseMsg);
+
+                // TODO: logic
+
+                Console.Write("\n");
+                outputFile.Write("\n");
+            }
+
+            inputFile.Dispose();
+            outputFile.Dispose();
+        }
+    }
+}
