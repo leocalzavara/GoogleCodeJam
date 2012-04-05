fe01153643863ed8ba240ee35bc79863c423b9ff
leocalzavara@gmail.com <leocalzavara@gmail.com@82dcad2b-1cc5-c1d1-2aa2-00431b11e489>
2012-04-05 04:37:52 +0000

    Projeto para resolver problema "C. T9 Spelling" do Qualification Round do Africa 2010.

---
+++ GoogleCodeJam/Africa2010.QualificationRound.C/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Africa2010.QualificationRound.C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
