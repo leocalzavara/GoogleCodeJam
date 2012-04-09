a88abcdabf68efb48771a97a02030aeafa11013f
leocalzavara@gmail.com <leocalzavara@gmail.com@82dcad2b-1cc5-c1d1-2aa2-00431b11e489>
2012-04-09 02:44:44 +0000

    Resolve o problema A do Qualification do Code Jam 2009.

--- GoogleCodeJam/CodeJam2009.Qualification.A/Program.cs
+++ GoogleCodeJam/CodeJam2009.Qualification.A/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace CodeJam2009.Qualification.A
 {
@@ -10,7 +11,7 @@ namespace CodeJam2009.Qualification.A
     {
         static void Main(string[] args)
         {
-            var inputFile = new StreamReader("SampleInput.txt");
+            var inputFile = new StreamReader("A-large-practice.in");
             var outputFile = new StreamWriter("Output.txt");
 
             string[] line = inputFile.ReadLine().Split(' ');
@@ -19,6 +20,13 @@ namespace CodeJam2009.Qualification.A
             int numberOfWords = Int32.Parse(line[1]);
             int numberOfCases = Int32.Parse(line[2]);
 
+            List<string> language = new List<string>();
+
+            for (int j = 0; j < numberOfWords; j++)
+            {
+                language.Add(inputFile.ReadLine());
+            }
+
             for (int i = 0; i < numberOfCases; i++)
             {
                 var caseMsg = String.Format("Case #{0}: ", i + 1);
@@ -26,7 +34,23 @@ namespace CodeJam2009.Qualification.A
                 Console.Write(caseMsg);
                 outputFile.Write(caseMsg);
 
-                // TODO: logic
+                int matches = 0;
+
+                string originalRegexString = inputFile.ReadLine();
+                string fixedRegexString = originalRegexString.Replace('(', '[').Replace(')', ']');
+
+                Regex regex = new Regex(fixedRegexString);
+
+                //Console.Write(regex.ToString());
+                
+                foreach (string word in language)
+                {
+                    if (regex.IsMatch(word))
+                        matches++;
+                }
+                
+                Console.Write(matches);
+                outputFile.Write(matches);
 
                 Console.Write("\n");
                 outputFile.Write("\n");
