2e17a6501375fc52412bec7ca6c6c1e5b403f9be
leocalzavara@gmail.com <leocalzavara@gmail.com@82dcad2b-1cc5-c1d1-2aa2-00431b11e489>
2012-04-13 05:40:38 +0000

    Resolve o problema small apenas.

---
+++ GoogleCodeJam/CodeJam2008.Round1A.A/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Oyster.Math;
+
+namespace CodeJam2008.Round1A.A
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Scalar_product_of_two_vectors_of_three_1s_should_be_3()
+        {
+            int[] x = { 1, 1, 1 };
+            int[] y = { 1, 1, 1 };
+
+            IntX scalarProduct = Program.ScalarProduct(x, y);
+
+            Assert.AreEqual(3, scalarProduct);
+        }
+
+        [Test]
+        public void Scalar_product_of_two_vectors_of_zeroes_should_be_0()
+        {
+            int[] x = { 0, 0, 0, 0, 0, 0 };
+            int[] y = { 0, 0, 0, 0, 0, 0 };
+
+            IntX scalarProduct = Program.ScalarProduct(x, y);
+
+            Assert.AreEqual(0, scalarProduct);
+        }
+    }
+}
