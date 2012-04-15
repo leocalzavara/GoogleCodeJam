7a21c9a7ce8e7141ea8c97d3c033e7dcd53e35c5
leocalzavara@gmail.com <leocalzavara@gmail.com@82dcad2b-1cc5-c1d1-2aa2-00431b11e489>
2012-04-15 00:41:47 +0000

    Versão do problema C que consegue processar o large input, mas deu incorreto o resultado.

--- GoogleCodeJam/CodeJam2012.Qualification.C/Program.cs
+++ GoogleCodeJam/CodeJam2012.Qualification.C/Program.cs
@@ -12,7 +12,7 @@ namespace CodeJam2012.Qualification.C
 
         static void Main(string[] args)
         {
-            var inputFile = new StreamReader("C-large.in");
+            var inputFile = new StreamReader("C-large-practice.in");
             var outputFile = new StreamWriter("Output.txt");
 
             int numberOfCases = Int32.Parse(inputFile.ReadLine());
@@ -27,8 +27,7 @@ namespace CodeJam2012.Qualification.C
                 lowerLimit = Int32.Parse(inputLine[0]);
                 higherLimit = Int32.Parse(inputLine[1]);
 
-                List<int[]> recycledPairs = new List<int[]>();
-                //int numberOfValidRecycledPairs = 0;
+                int numberOfValidRecycledPairs = 0;
 
                 for (int j = lowerLimit; j <= higherLimit; j++)
                 {
@@ -41,21 +40,18 @@ namespace CodeJam2012.Qualification.C
                         int m = Int32.Parse(currentNumber.Substring(k) + currentNumber.Substring(0, k));
                         //Console.Write(number + " ");
 
-                        int[] recycledPair = { n, m };
-
-                        if (IsRecycledPair(recycledPair) && !recycledPairs.Contains(recycledPair))
+                        if (IsRecycledPair(n, m))
                         {
-                            recycledPairs.Add(recycledPair);
-                            //numberOfValidRecycledPairs++;
-                            //outputFile.WriteLine(recycledPair[0] + ", " + recycledPair[1]);
+                            numberOfValidRecycledPairs++;
+                            //outputFile.WriteLine(n + ", " + m);
                         }
                     }
 
                     //Console.Write(Environment.NewLine);
                 }
 
-                Console.Write(recycledPairs.Count + Environment.NewLine);
-                outputFile.Write(recycledPairs.Count + Environment.NewLine);
+                Console.Write(numberOfValidRecycledPairs + Environment.NewLine);
+                outputFile.Write(numberOfValidRecycledPairs + Environment.NewLine);
                 outputFile.Flush();
             }
 
@@ -63,11 +59,8 @@ namespace CodeJam2012.Qualification.C
             outputFile.Dispose();
         }
 
-        static bool IsRecycledPair(int[] recycledPair)
+        static bool IsRecycledPair(int n, int m)
         {
-            int n = recycledPair[0];
-            int m = recycledPair[1];
-
             if (lowerLimit <= n && n < m && m <= higherLimit)
             {
                 return true;
