32dd86e74379dfe7100e0371560a921498e454dd
Leonardo Calzavara <leocalzavara@gmail.com>
2015-04-10 22:48:55 -0300

    Program for CodeJam2015 Qualification Round, A - Standing Ovation

---
+++ CodeJam2015.Qualification.A/Output.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CodeJam2015.Qualification.A
+{
+    public class Output
+    {
+        private bool _debug;
+        private string _fileName;
+        private StreamWriter _outputFile;
+
+        public Output() : this("Output.txt", false) { }
+
+        public Output(string fileName, bool debug)
+        {
+            this._debug = debug;
+            this._fileName = fileName;
+            this._outputFile = new StreamWriter(_fileName);
+        }
+
+        public void Write(string value)
+        {
+            if (_debug)
+            {
+                Console.Write(value);
+            }
+
+            this._outputFile.Write(value);
+        }
+
+        public void Write(int value)
+        {
+            if (_debug)
+            {
+                Console.Write(value);
+            }
+
+            this._outputFile.Write(value);
+        }
+
+        public void WriteLine(string format, params object[] args)
+        {
+            if (_debug)
+                Console.WriteLine(format, args);
+
+            this._outputFile.WriteLine(format, args);
+        }
+
+        public void Flush()
+        {
+            this._outputFile.Flush();
+        }
+
+        public void Dispose()
+        {
+            this._outputFile.Dispose();
+        }
+    }
+}
---
+++ CodeJam2015.Qualification.A/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJam2015.Qualification.A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFile = new StreamReader("A-small-attempt0.in");
+            var output = new Output("Output.txt", true);
+
+            int numberOfCases = Int32.Parse(inputFile.ReadLine());
+
+            for (int caseNumber = 1; caseNumber <= numberOfCases; ++caseNumber)
+            {
+                var inputLine = inputFile.ReadLine().Split(' ');
+                var Smax = Int32.Parse(inputLine[0]);
+                var S = inputLine[1].ToCharArray().Select(i => Int32.Parse(i.ToString())).ToArray();
+
+                long numberOfStandUpPeople = 0;
+                long numberOfFriendsToCall = 0;
+
+                for (int i = 0; i <= Smax; ++i)
+                {
+                    if (S[i] == 0) continue;
+
+                    if (numberOfStandUpPeople < i)
+                    {
+                        numberOfFriendsToCall += (i - numberOfStandUpPeople);
+                        numberOfStandUpPeople += (i - numberOfStandUpPeople);
+                    }
+                    
+                    numberOfStandUpPeople += S[i];
+                }
+
+                output.WriteLine("Case #{0}: {1}", caseNumber, numberOfFriendsToCall);
+                output.Flush();
+            }
+
+            output.Dispose();
+        }
+    }
+}
